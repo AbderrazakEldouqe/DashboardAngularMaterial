edc0a129626f591719ca68f05ef8371830e0c740
eldouqe <abderrazak.eldouqe1@gmail.com>
2020-05-10 03:40:30 +0200

    first commit

---
+++ DashboardApp/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren:()=> import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'home',
+    loadChildren:()=> import('./modules/navigation/navigation.module').then(m => m.NavigationModule)
+   
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
